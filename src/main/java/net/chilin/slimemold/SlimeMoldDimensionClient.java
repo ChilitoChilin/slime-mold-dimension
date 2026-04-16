a59cd600addd1083d59de484139a3d2120869778
Chilin <myckelmalave@gmail.com>
2026-04-15 22:48:28 -0400

    Initial commit

---
+++ src/main/java/net/chilin/slimemold/SlimeMoldDimensionClient.java
@@ -0,0 +1,10 @@
+package net.chilin.slimemold;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SlimeMoldDimensionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/chilin/slimemold/SlimeMoldDimensionDataGenerator.java
@@ -0,0 +1,11 @@
+package net.chilin.slimemold;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SlimeMoldDimensionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
